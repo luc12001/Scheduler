d32e8502373fa18b8ee5f560652e3e3c855a6225
luc12001 <luc12001@byui.edu>
2022-03-22 16:52:17 -0500

    added setAppointment method

--- controllers/doctor.js
+++ controllers/doctor.js
@@ -29,4 +29,60 @@ exports.editNotes = (req, res, next) => {};
 
 exports.editAvailability = (req, res, next) => {};
 
-exports.setAppointment = (req, res, next) => {};
+exports.setAppointment = (req, res, next) => {
+    const patientId = req.body.patientId;
+    const patientName = req.body.patientName;
+    const patientNotes = req.body.patientNotes;
+    const appTime = req.body.appTime;
+    if (!image) {
+        return res.status(422).render('/users/receptionist/setAppointment', {
+            pageTitle: 'Set Appointment',
+            path: '/users/receptionist/setAppointment',
+            editing: false,
+            hasError: true,
+            product: {
+                patientId: patientId,
+                patientName: patientName,
+                patientNotes: patientNotes,
+                appTime: appTime
+            }
+        });
+    }
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        console.log(errors.array());
+        return res.status(422).render('/users/receptionist/setAppointment', {
+            pageTitle: 'Set Appointment',
+            path: '/users/receptionist/setAppointment',
+            editing: false,
+            hasError: true,
+            product: {
+                patientId: patientId,
+                patientName: patientName,
+                patientNotes: patientNotes,
+                appTime: appTime
+            },
+            errorMessage: errors.array()[0].msg,
+            validationErrors: errors.array()
+        });
+    }
+
+    const newAppointment = new setAppointment({
+        patientId: patientId,
+        patientName: patientName,
+        patientNotes: patientNotes,
+        appTime: appTime
+    });
+    newAppointment
+        .save()
+        .then(result => {
+            console.log('Appointment Set');
+            res.redirect('/users/receptionist/setAppointment');
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
+};
