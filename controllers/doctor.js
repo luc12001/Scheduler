482a303e4062408c35f7da16a3d5503d457b7b0d
luc12001 <luc12001@byui.edu>
2022-03-22 17:02:52 -0500

    added editAvailability Method for Doctor

--- controllers/doctor.js
+++ controllers/doctor.js
@@ -27,7 +27,48 @@ exports.addNotes = (req, res, next) => {
 
 exports.editNotes = (req, res, next) => {};
 
-exports.editAvailability = (req, res, next) => {};
+exports.editAvailability = (req, res, next) => {
+    const doctorId = req.body.doctorId;
+    const doctorName = req.body.doctorName;
+    const doctorAvailability = req.body.doctorAvailability;
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).render('users/doctor/edit-availability', {
+            pageTitle: 'Edit Doctor Availability',
+            path: 'users/doctor/edit-availability',
+            editing: true,
+            hasError: true,
+            appointment: {
+                doctorName: doctorName,
+                doctorAvailability: doctorAvailability,
+                _id: doctorId
+            },
+            errorMessage: errors.array()[0].msg,
+            validationErrors: errors.array()
+        });
+    }
+
+    Doctor.findById(doctorId)
+        .then(doctor => {
+            if (doctor.userId.toString() !== req.user._id.toString()) {
+                return res.redirect('/');
+            }
+            doctor.doctorName = doctorName;
+            doctor.doctorNotes = doctorNotes;
+            doctor.doctorAvailability = req.body.doctorAvailability;
+            return product.save().then(result => {
+                console.log('UPDATED AVAILABILITY!');
+                res.redirect('users/doctor/edit-availability');
+            });
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
+};
 
 exports.setAppointment = (req, res, next) => {
     const patientId = req.body.patientId;
