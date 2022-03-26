91f741a6321ac4266c896352c0f0deb7ada47148
luc12001 <luc12001@byui.edu>
2022-03-25 19:50:22 -0500

    added edit doctor availability and edited others

--- controllers/receptionist.js
+++ controllers/receptionist.js
@@ -60,7 +60,7 @@ exports.editAppointment = (req, res, next) => {
             patient.patientName = patientName;
             patient.patientNotes = patientNotes;
             patient.appTime = appTime;
-            return product.save().then(result => {
+            return patient.save().then(result => {
                 console.log('UPDATED APPOINTMENT!');
                 res.redirect('/receptionist/appointments');
             });
@@ -87,4 +87,42 @@ exports.approveRequest = (req, res, next) => {};
 
 exports.denyRequest = (req, res, next) => {};
 
-exports.editDoctorAvailability = (req, res, next) => {};
+exports.editDoctorAvailability = (req, res, next) => {const patientId = req.body.patientId;
+    const doctorName = req.body.doctorName;
+    const doctorAvailability = req.body.doctorAvailability;
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).render('users/edit-doctor-availability', {
+            pageTitle: 'Edit Doctor Availability',
+            path: '/receptionist/edit-doctor-availability',
+            editing: true,
+            hasError: true,
+            appointment: {
+                doctorName: doctorName,
+                doctorAvailability: doctorAvailability,
+                _id: doctorId
+            },
+            errorMessage: errors.array()[0].msg,
+            validationErrors: errors.array()
+        });
+    }
+
+    Doctor.findById(doctorId)
+        .then(doctor => {
+            if (doctor.userId.toString() !== req.user._id.toString()) {
+                return res.redirect('/');
+            }
+            doctor.doctorName = doctorName;
+            doctor.doctorAvailability = doctorAvailability;
+            return doctor.save().then(result => {
+                console.log('UPDATED DOCTOR AVAILABILITY!');
+                res.redirect('/users/edit-doctor-availability');
+            });
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });};
