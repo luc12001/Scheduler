9608800e1b81dbf08712eaab41330253b56413ea
Scubadoo2 <scubadoo2@rocketmail.com>
2022-03-30 10:28:53 -0700

    Setting up Login and Signup pages

--- models/user.js
+++ models/user.js
@@ -18,7 +18,8 @@ const userSchema = new mongoose.Schema({
         lastName: { type: String, required: true },
         email: { type: String, required: true },
         phone: {type: String, required: true},
-        address: {type: String, required: true}
+        address: {type: String, required: true},
+        mail: {type: String, required: true}
     }
 
 });
--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,10 @@ const router = express.Router();
 const authController = require("../controllers/auth");
 
 router.get('/login', authController.logInGet);
+router.post("/login", authController.logInPost);
+
 router.get('/signup', authController.signUpGet);
+router.get("/signup", authController.signUpPost);
 
 
 module.exports = router;
